a26c2c02e4ebd17cb2099e579917cc4cfabfc61c
austinrivas <1114054+austinrivas@users.noreply.github.com>
2023-02-19 02:22:43 +0700

    remove react ssr

--- scripts/fetch-data.ts
+++ scripts/fetch-data.ts
@@ -19,8 +19,8 @@ async function formatRow(row: IGSheetRow): Promise<IGSheetRow> {
     const src = row.c[2].v as string
     const id = getImageId(src)
     const downloadSrc = `https://drive.google.com/uc?export=download&id=${id}`
-    const rawDest = `./images/raw/${id}.jpg`
-    const processedDest = `./images/processed/${id}.jpg`
+    const rawDest = `./images/${id}.jpg`
+    const processedDest = `./build/images/${id}.jpg`
     await downloadImage(downloadSrc, rawDest)
     await resizeImage(rawDest, processedDest)
     const base64Image = await convertImageToBase64(processedDest)
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import ReactDOM, { hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootElement)
-if (rootElement.hasChildNodes()) {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-} else {
-  hydrateRoot(rootElement, 
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

6ce6361f4d4e197340ec3413e0650ae5d6d7bcc8
Austin Rivas <1114054+austinrivas@users.noreply.github.com>
2023-02-18 07:07:56 +0000

    added swiper demo content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination } from "swiper"
+import init from './gsheet-client'
+import "swiper/css"
+import "swiper/css/pagination"
+import "./App.css"
 
-function App() {
+export default function App() {
+  const data = init()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <Swiper
+        className="menu-swiper swiper-h"
+        spaceBetween={50}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>Demo - Welcome Screen</SwiperSlide>
+        <SwiperSlide>
+          <Swiper
+            className="entre-swiper swiper-v"
+            direction={"vertical"}
+            spaceBetween={50}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+          >
+            <SwiperSlide>Entre 1</SwiperSlide>
+            <SwiperSlide>Entre 2</SwiperSlide>
+            <SwiperSlide>Entre 3</SwiperSlide>
+            <SwiperSlide>Entre 4</SwiperSlide>
+            <SwiperSlide>Entre 5</SwiperSlide>
+          </Swiper>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Swiper
+            className="drink-swiper swiper-v"
+            direction={"vertical"}
+            spaceBetween={50}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+          >
+            <SwiperSlide>Drink 1</SwiperSlide>
+            <SwiperSlide>Drink 2</SwiperSlide>
+            <SwiperSlide>Drink 3</SwiperSlide>
+            <SwiperSlide>Drink 4</SwiperSlide>
+            <SwiperSlide>Drink 5</SwiperSlide>
+          </Swiper>
+        </SwiperSlide>
+        <SwiperSlide>Social Screen</SwiperSlide>
+        <SwiperSlide>Kate's Place Promo</SwiperSlide>
+        <SwiperSlide>Other Services</SwiperSlide>
+      </Swiper>
     </div>
-  );
+  )
 }
-
-export default App;
